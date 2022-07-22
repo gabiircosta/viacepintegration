5b22fbbbd27199e8526491ce0cad55de573ac2d7
grcos <gr.costa@outlook.com>
2022-07-22 16:13:17 -0300

    integração api viacep

---
+++ viacepintegration/src/main/java/com/viacepintegration/ViacepintegrationApplication.java
@@ -0,0 +1,13 @@
+package com.viacepintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViacepintegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViacepintegrationApplication.class, args);
+	}
+
+}
---
+++ viacepintegration/src/test/java/com/viacepintegration/ViacepintegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viacepintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViacepintegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
